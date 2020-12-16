5045ded1924f8b405ee9dacd7af482d38b3de004
Taylor Bustamante <taylorbustamante16@gmail.com>
2020-12-16 11:39:48 -0800

    commit tree 6f386c549b2daf44be92abbc300f49d7f7f85d9a

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -9,8 +9,19 @@ class RecipesController < ApplicationController
 
   def new
     @recipe = Recipe.new
+    @recipe.ingredients.build()
+    @recipe.ingredients.build()
   end
 
   def create
+    Recipe.create(recipe_params)
+  end
+
+  private
+  def recipe_params
+    params.require(:recipe).permit(:title, ingredient_attributes: [
+      :name,
+      :quantity
+    ])
   end
 end
